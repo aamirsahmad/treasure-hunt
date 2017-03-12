7a698802d5e3c980ac19facc2223a6112a5fa298
Aamir Ahmad <aamirahmad@outlook.com>
2017-03-11 21:34:16 -0500

    fix error handling

--- app.js
+++ app.js
@@ -190,15 +190,15 @@ if (process.env.DEV === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 /**
  * Start Express server.
